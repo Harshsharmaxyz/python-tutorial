0dca29a1d2e33144e91aa1016b446a4ab64c5c2b
Harsh Sharma xyz <hs5586528@gmail.com>
2024-09-27 18:25:54 +0530

    comments

---
+++ firstcode.py
@@ -0,0 +1,15 @@
+#standardoutput
+#print()
+#n number of parameter
+#bydefault line change
+
+print("welcome")
+'''print("welcome")
+print("welcome")
+print("welcome")
+print("welcome")'''
+print("welcom,","the","world")
+print("welcom,","the"""+""+"world")
+print("welcome")
+print("rrsf")
+print("world")
