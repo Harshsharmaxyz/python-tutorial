6aa87da7745552a9f1c6616241da42d64b2042cb
Harsh Sharma xyz <hs5586528@gmail.com>
2024-09-28 15:46:55 +0530

    secondtoken

---
+++ secondclass.py
@@ -0,0 +1,30 @@
+#end parameter
+print("welcome" , end =" ")
+print("to" , end =" ")
+print("bhopal", end =" ")
+print("cybrom")
+
+
+#sep parameter seperate
+print("welcome","to","python",sep=" $ ")
+print("welcome","to","python",sep=" end ")
+print("welcome","to","python",sep=" 980 ")
+
+#indent
+#-------
+#to difine block in python we use python we use indent,
+#it is equivalent to {} in c++,
+#      python----->indent
+
+print("left most cornor")
+print("aaaaaaa")
+if True:
+    print("aaaaaaaa")
+
+#/backsalsc
+print("sjnbvbvonevnevenvnrrunununvnvnrni\
+     eovjneovunevneovunnvenvervivjevijevju\
+    elvkmerveovelkpvoiemrjf")
+print("sjnbvbvonevnevenvnrrunununvnvnrni"
+     "eovjneovunevneovunnvenvervivjevijevju"
+    "elvkmerveovelkpvoiemrjf")
---
+++ secondclasstoken.py
@@ -0,0 +1,27 @@
+#tokens:
+#smallest unit of code which has a meaining to translator
+
+print("welcome")  # token
+
+# print is indentifier
+#"welcome" literals(string)
+# # comment
+# this program in have 2 tokes
+
+################################################
+#five type tokens
+#1 keyword
+#2 identifier
+#3 literal
+#4 special symbols(delimiter)
+#5 operator
+
+#######################
+#keyword are reserved word  which has a special meaning to compiler.
+import keyword
+print(keyword.kwlist)
+print("count",len(keyword.kwlist))
+
+
+#identifier
+# to identifier the component of programmmer uniquely,  we use identifier
