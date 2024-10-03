eee71d2d53c206c251ee405262259bdb32869944
Harsh Sharma xyz <hs5586528@gmail.com>
2024-10-03 15:36:42 +0530

    operator

---
+++ thirdclass.py
@@ -0,0 +1,45 @@
+#ARTHMATIC OPERATOR
+# +,-,*,/,%,//,**
+print(23 % 4)# moduls operator
+print(4%23)#moduls operator
+
+# floor divison operator: (//)
+print(23/4)#5.75
+# small interger value is floor 
+# so much use floor divisible 
+print(60//7)#8
+print(60//6)#10
+#floor divisible value
+
+# exponetail operator **
+print(4**5) # 4 ki power 5 * karenge toh answer aayega  
+print(7**3) # 343
+#=========================================================================
+
+# comparison operator
+# <,>,<=,>=,==,!=                                  # A = 65  ,a= 95  ,0=48:
+                                                     
+print(23<25) #true
+print(23>25) #false
+print(23==25) #false
+print(23!=25) #true
+print(23<=25) #true
+print(23>=25) #false
+
+#============================================================================
+#string comparison
+print("apple">"banana")#false                 
+print("appletttt">"banana")#false
+# apple ka a= 65: banana ka b=98
+# B=66,ask value
+print("apple" > "applf")# false
+print("harsh"=="harsh")#True
+print("harsh"!="harsh")#flase
+
+# on the basis of ASCII (american standard code for information interchange)
+# in lexicographical manner
+
+print("oranGe" < "orange") # true
+print("ABCD" < "ABC") # false
+
+#logical operator
